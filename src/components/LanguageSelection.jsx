506c420c4bd15de2b5fad707e206d78fea3dbd5e
Rebecca Noy <rebecca.noy@gmx.de>
2023-11-28 13:41:12 +0100

    replaced mock data with real fetch for language selection

--- src/components/LanguageSelection.jsx
+++ src/components/LanguageSelection.jsx
@@ -1,14 +1,31 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import { useParams, useNavigate } from "react-router"
 import { mockData } from "../api/mockData"
+import { baseURL } from "../helpers/helpers"
 
 export default function LanguageSelection () {
+
   const navigate = useNavigate()
+  const [categories, setCategories] = useState(null)
+
+  const getCategories = () => {
+    const endpoint = "/categories"
+
+    fetch(baseURL + endpoint)
+      .then(response => response.json())
+      .then(data => setCategories(data))
+      .catch(error => console.log("error loading categories", error))
+  }
+
+  useEffect(getCategories, [])
+
+  if (!categories) return "Loading…"
+
   return (
     <>
       <h2>Select your category</h2>
       <div className="autoColumns">
-        {mockData.categories.map((category, index) => <label key={index} onClick={() => navigate("/practice/"+index+"/0")}>{category.title}</label>)}
+        {categories.map((category, index) => <label key={index} onClick={() => navigate("/practice/"+index+"/0")}>{category.title}</label>)}
       </div>
     </>
   )
