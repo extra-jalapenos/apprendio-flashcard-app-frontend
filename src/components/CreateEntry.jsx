dccb2752da9c235ea65c601d10efa06fdceae895
Rebecca Noy <rebecca.noy@gmx.de>
2023-11-29 17:14:13 +0100

    functionality for posting new entry

--- src/components/CreateEntry.jsx
+++ src/components/CreateEntry.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react"
-import { baseURL } from "../helpers/constants"
+import { baseURL, entryBlueprint } from "../helpers/constants"
 
 function DropdownField ({category}) {
   const {id, title} = category
@@ -29,15 +29,40 @@ export default function CreateEntry() {
   const [form, setForm] = useState(initForm)
 
   const handleInput = (event) => {
-    const { name, value } = event.target 
-    console.log(name, value)
+    const { name, value } = event.target
+    setForm({ ...form, [name]:value })
   }
 
   const handleSubmit = (event) => {
     event.preventDefault()
-    if (!form.answer || !form.question) return
-    console.log(form)
-    
+    // if (!form.answer || !form.question) return
+    // if (form.answer && form.question) 
+    console.log("post")
+
+    const endpoint = "/entries"
+    const headers = {
+      "content-type": "application/json"
+    }
+
+    const category = categories.find(category => category.title === form.category)
+
+    const body = { ...entryBlueprint,
+      "categoryId": category.id,
+      "last": new Date().toISOString,
+      "question": form.question,
+      "answer": form.question,
+    }
+
+    const options = {
+      method: "POST",
+      headers: headers,
+      body: JSON.stringify(body)
+    }
+
+    fetch(baseURL + endpoint, options)
+      .then(response => response.json())
+      .then(data => console.log(data))
+      .catch(error => console.log("error creating entry", error))
   }
   
   if (!categories) return (<p>Loading…</p>)
@@ -45,14 +70,15 @@ export default function CreateEntry() {
   return (
     <form className="twoColumns" onChange={handleInput} onSubmit={handleSubmit}>
       <label>Category</label>
-      <select name="categoryId">
-        {categories.map((category, index) => <DropdownField category={category} />)}
+      <select name="category">
+        {categories.map((category, index) => <DropdownField key={index} category={category}/>)}
       </select>
       <label>Question</label>
-      <textarea type="text" name="question" />
+      <textarea type="text" name="question" required/>
       <label>Answer</label>
-      <textarea type="text" name="answer" />
-      <button value="Submit">Create</button>
+      <textarea type="text" name="answer" required/>
+      <p></p>
+      <button value="Submit">➕ Create</button>
     </form>
   )
 }
--- src/helpers/constants.js
+++ src/helpers/constants.js
@@ -1,1 +1,13 @@
 export const baseURL = "http://localhost:3000"
+
+export const entryBlueprint = {
+  "categoryId": "",
+  "stage": 0,
+  "last": "",
+  "repetitions": {
+    "correct": 0,
+    "wrong": 0
+  },
+"question": "", 
+"answer": ""
+}
