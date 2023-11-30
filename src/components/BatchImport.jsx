38272d6f3099a9e988e609a1aff46d968cd5975f
Rebecca Noy <rebecca.noy@gmx.de>
2023-11-30 23:33:45 +0100

    choosing file works and reading out too

--- src/components/BatchImport.jsx
+++ src/components/BatchImport.jsx
@@ -1,7 +1,30 @@
+import { useState } from "react"
+
 export default function BatchImport () {
+  const [file, setFile] = useState(null)
+  const [data, setData] = useState(null)
+
+  const handleInput = (event) => setFile(event.target.files[0])
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    if (!file) return
+
+    const reader = new FileReader();
+    reader.onload = (e) => {
+      const fileContents = e.target.result;
+      const dataArray = fileContents.split("\n");
+      setData(dataArray);
+      console.log(dataArray.length)
+    };
+
+    reader.readAsText(file);
+  }
+
   return (
-    <>
+    <form onSubmit={handleSubmit}>
+    <input type="file" accept="text/csv, image/jpeg" onInput={handleInput}/>
     <button>Start import</button>
-    </>
+    </form>
   )
 }
