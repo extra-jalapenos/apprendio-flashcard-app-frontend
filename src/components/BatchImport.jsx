c46bc04a092692453f9a2e356c1b2aaf89e58f2d
Rebecca Noy <rebecca.noy@gmx.de>
2023-11-30 23:56:44 +0100

    import / reading xlsx works now, too

--- src/components/BatchImport.jsx
+++ src/components/BatchImport.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import * as XLSX from "xlsx";
 
 export default function BatchImport () {
   const [file, setFile] = useState(null)
@@ -6,6 +7,7 @@ export default function BatchImport () {
 
   const handleInput = (event) => setFile(event.target.files[0])
 
+  // CSV
   const handleSubmit = (event) => {
     event.preventDefault()
     if (!file) return
@@ -15,15 +17,34 @@ export default function BatchImport () {
       const fileContents = e.target.result;
       const dataArray = fileContents.split("\n");
       setData(dataArray);
-      console.log(dataArray.length)
+      console.log(dataArray)
     };
 
     reader.readAsText(file);
   }
 
+  // XLSX
+  const handleSubmitXLSX = (event) => {
+    event.preventDefault();
+    if (!file) return;
+
+    const reader = new FileReader();
+    reader.onload = (e) => {
+      const fileContents = e.target.result;
+      const workbook = XLSX.read(fileContents, { type: "binary" });
+      const sheetName = workbook.SheetNames[0]; // Assuming you're interested in the first sheet
+      const sheet = workbook.Sheets[sheetName];
+      console.log(sheetName, sheet)
+      const dataArray = XLSX.utils.sheet_to_json(sheet, { header: 1 });
+      setData(dataArray);
+    };
+
+    reader.readAsBinaryString(file);
+  };
+
   return (
-    <form onSubmit={handleSubmit}>
-    <input type="file" accept="text/csv, image/jpeg" onInput={handleInput}/>
+    <form onSubmit={handleSubmitXLSX}>
+    <input type="file" accept="text/csv *.xlsx" onInput={handleInput}/>
     <button>Start import</button>
     </form>
   )
