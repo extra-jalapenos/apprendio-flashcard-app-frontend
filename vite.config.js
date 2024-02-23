c519366f0b862c2cd3673a8ed88bffe6a3f3dcdc
AllyDouillette <142409831+AllyDouillette@users.noreply.github.com>
2024-02-23 21:31:08 +0100

    add proxy for calls to server

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { baseURL } from './src/helpers/constants.js'
+console.log(baseURL)
 
-// https://vitejs.dev/config/
 export default defineConfig({
   server:{
     proxy:{
       '/api': {
-        target: 'http://localhost:5173',
+        target: baseURL,
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\//, '')
+        rewrite: (path) => path.replace(/^\/api/, ''),
       }
     }
   },
