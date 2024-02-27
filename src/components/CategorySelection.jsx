55fecfd5f4a5dc57df5054e64157792ab16e28e0
AllyDouillette <142409831+AllyDouillette@users.noreply.github.com>
2024-02-27 20:26:17 +0100

    add connection to database for categories

--- src/components/CategorySelection.jsx
+++ src/components/CategorySelection.jsx
@@ -1,27 +1,36 @@
 import { useState, useEffect } from "react"
-import { useParams, useNavigate } from "react-router"
-import { baseURL, header } from "../helpers/constants"
+import { useNavigate } from "react-router"
 
 export default function LanguageSelection () {
 
   const navigate = useNavigate()
   const [categories, setCategories] = useState(null)
 
-  const getCategories = async () => {
-    try {
-      const res = await fetch("/api/categories")
-      const data = res.json()
-      if (res.status === 200) {
-        setCategories(data.categories)
-      } else {
-        setCategories([])
+  const getCategories = () => {
+    const response = async () => {
+      try {
+        console.log("test1")
+        const myHeaders = new Headers()
+        const token = sessionStorage.getItem("token")
+        myHeaders.set("Authorization", `Bearer ${token}`)
+        const options = {
+          headers: myHeaders
+        }
+        const res = await fetch("/api/users/me/categories", options)
+        const data = await res.json()
+        if (res.status === 200) {
+          setCategories(data.categories)
+        } else {
+          console.log(data)
+        }
+      } catch (error) {
+        console.log("error fetching categories")
       }
-    } catch (error) {
-      console.log("error fetching categories")
     }
+    response()
   }
 
-  useEffect(() => getCategories(), [])
+  useEffect(getCategories, [])
 
   if (!categories) return (<div className="center">
       Loading…
@@ -31,7 +40,7 @@ export default function LanguageSelection () {
     <>
       <h2 className="center">Select your category</h2>
       <div className="autoColumns">
-        {categories.map((category, index) => <label key={index} onClick={() => navigate("/practice/"+category.id+"/")}>{category.title}</label>)}
+        {categories.map((category, index) => <label key={index} onClick={() => navigate("/practice/"+category.id+"/")}>{category.name}</label>)}
         <button onClick={() => navigate("/create-category")}>Create new category</button>
       </div>
     </>
