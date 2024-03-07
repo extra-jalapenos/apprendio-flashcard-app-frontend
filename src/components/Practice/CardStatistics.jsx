960c6b66f09d55043b5997c124389a0c4869d9c7
AllyDouillette <142409831+AllyDouillette@users.noreply.github.com>
2024-03-07 12:17:41 +0100

    modify card stats component to new structure

--- src/components/Practice/CardStatistics.jsx
+++ src/components/Practice/CardStatistics.jsx
@@ -1,21 +1,20 @@
-import { useParams } from "react-router"
 import { maxLevel } from "../../helpers/constants"
 
-export default function CardStats ({props}) {
-  const { repetitions, level, lastAskedAt } = props
-  const { cardId } = useParams()
-  if (!cardId) return
+export default function CardStats ({card}) {
+  console.log(card)
+  const { repetitions, level, lastAskedAt, id } = card
+  if (!id) return
   return (
     <>
-    <h3>Card {cardId}</h3>
-    <div className="buttoncontainer">
-        {lastAskedAt && <label>Last known at {new Date(lastAskedAt).toLocaleString()}</label>}
-        <p className="circlebutton">{repetitions ? (level / repetitions * 100).toFixed(0) : "0"}%</p>
-        <label>Total</label>
-        <p className="circlebutton">{repetitions}</p>
-        <label>Level</label>
-        <p className={level >= maxLevel ? "circlebutton green" : "circlebutton"}>{level}</p>
-    </div>
+      <h3>Card {id}</h3>
+      <div className="buttoncontainer">
+          {lastAskedAt && <label>Last known at {new Date(lastAskedAt).toLocaleString()}</label>}
+          <p className="circlebutton">{repetitions ? (level / repetitions * 100).toFixed(0) : "0"}%</p>
+          <label>Total</label>
+          <p className="circlebutton">{repetitions}</p>
+          <label>Level</label>
+          <p className={level >= maxLevel ? "circlebutton green" : "circlebutton"}>{level}</p>
+      </div>
     </>
   )
 }
