81dbc5fe29390d8148d74936d76f41741d2a0202
AllyDouillette <142409831+AllyDouillette@users.noreply.github.com>
2024-03-11 16:19:55 +0100

    render footer only if logged in

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,6 +1,11 @@
 import SessionStatistics from "./SessionStatistics"
+import { userContext } from "../../context"
+import { useContext } from "react"
 
 export default function Footer () {
+  const { user } = useContext(userContext)
+  if (!user) return
+  
   return (
     <footer>
       <SessionStatistics />
