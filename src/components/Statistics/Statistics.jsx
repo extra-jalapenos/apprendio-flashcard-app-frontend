c8a090f5032804320e7f23bb6176c256b525ef68
AllyDouillette <142409831+AllyDouillette@users.noreply.github.com>
2024-03-11 15:06:14 +0100

    finetune rendering of empty statistics

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -5,17 +5,25 @@ function Entry ({statisticObj}) {
   const date = new Date(statisticObj.date).toLocaleDateString()
   const { correct, incorrect } = statisticObj
   const total = Number(correct) + Number(incorrect)
+  if (total <= 0) {
+    return (
+      <div className="listentry autoColumns fourColumns">
+        <p>{date}</p>
+      </div>
+    )
+  }
+  
   return (
     <div className="listentry autoColumns fourColumns">
     <p>{date}</p>
     <div className="autoColumns threeColumns">
       <span>{correct}</span>
-      <span className="circlebutton green" style={{opacity: correct / total}} />
+      <span className="circlebutton green" style={{opacity: total ? correct / total : 0}} />
       <span>{(correct / total * 100).toFixed(0)+"%"}</span>
     </div>
     <div className="autoColumns threeColumns">
       <span>{incorrect}</span>
-      <span className="circlebutton red" style={{opacity: incorrect / total}} />
+      <span className="circlebutton red" style={{opacity: total ? incorrect / total : 0}} />
       <span>{(incorrect / total * 100).toFixed(0)+"%"}</span>
     </div>
       <span className="circlebutton blue">{total}</span>
