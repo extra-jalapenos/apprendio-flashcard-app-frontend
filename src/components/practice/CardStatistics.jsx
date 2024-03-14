199b4929781a7095d4094d0e35cd76ebf56c86f9
AllyDouillette <142409831+AllyDouillette@users.noreply.github.com>
2024-03-14 18:06:40 +0100

    optimize css for practice view

--- src/components/practice/CardStatistics.jsx
+++ src/components/practice/CardStatistics.jsx
@@ -1,4 +1,5 @@
 import { maxLevel } from "../../helpers/constants"
+import "./style.css"
 
 export default function CardStats ({card}) {
   const { repetitions, level, lastAskedAt, id } = card
@@ -6,13 +7,15 @@ export default function CardStats ({card}) {
   return (
     <>
       <h3>Card {id}</h3>
-      <div className="buttoncontainer">
-        {lastAskedAt && <label>Last known at {new Date(lastAskedAt).toLocaleString()}</label>}
-        <p className="circlebutton">{repetitions ? (level / repetitions * 100).toFixed(0) : "0"}%</p>
-        <label>Total</label>
-        <p className="circlebutton">{repetitions}</p>
-        <label>Level</label>
-        <p className={level >= maxLevel ? "circlebutton green" : "circlebutton"}>{level}</p>
+      <div>
+        <div className="buttoncontainer">
+          {lastAskedAt && <label id="lastKnown">Last known: {new Date(lastAskedAt).toLocaleDateString()}</label>}
+          <button id="percent">{repetitions ? (level / repetitions * 100).toFixed(0) : "0"}%</button>
+          <label>Repetitions</label>
+          <p className="circlebutton">{repetitions}</p>
+          <label>Level</label>
+          <p className={level >= maxLevel ? "circlebutton green" : "circlebutton"}>{level}</p>
+        </div>
       </div>
     </>
   )
