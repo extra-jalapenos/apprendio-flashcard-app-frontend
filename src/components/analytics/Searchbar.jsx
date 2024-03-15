57987eeb33e79416fb8ab588e268d5eb84792030
AllyDouillette <142409831+AllyDouillette@users.noreply.github.com>
2024-03-15 14:03:12 +0100

    change text slightly

--- src/components/analytics/Searchbar.jsx
+++ src/components/analytics/Searchbar.jsx
@@ -1,8 +1,7 @@
 export default function Searchbar ({handleInput}) {
   return (
     <form className="center">
-      <label>Search entries for text</label>
-      <input type="text" className="searchbar" placeholder="Search here…" onChange={handleInput} />
+      <input type="text" className="searchbar" placeholder="Search in cards" onChange={handleInput} />
     </form>
   )
 }
