b18bb3eb50f5659e816f85185b0896c8662a6a78
AllyDouillette <142409831+AllyDouillette@users.noreply.github.com>
2024-03-16 20:30:36 +0100

    lernmausi changed from imprint

--- src/components/imprint/Imprint.jsx
+++ src/components/imprint/Imprint.jsx
@@ -2,14 +2,14 @@ export default function Imprint() {
   return (
     <div>
       <h2>Imprint</h2>
-      <p>This imprint is valid for the domain lernmausi.de and all subdomains.</p>
-      <h3>Information due to § 5 TMG</h3>
+      <p>This imprint is valid for the domain apprendio.online and all subdomains.</p>
+      {/* <h3>Information due to § 5 TMG</h3>
       <h4>Adress</h4>
       <p>Rebecca Noy<br/>
       Brentanostraße 5<br/>
-      93051 Regensburg</p>
+      93051 Regensburg</p> */}
       <h4>Contact</h4>
-      <p>For technical feedback as well as other non-spammy stuff, please bother me at <a href="mailto:info@lernmausi.de">info@lernmausi.de</a>.</p>
+      <p>For technical feedback as well as other non-spammy stuff, please bother me at <a href="mailto:info@apprendio.online">info@apprendio.online</a>.</p>
     </div>
   )
 }
