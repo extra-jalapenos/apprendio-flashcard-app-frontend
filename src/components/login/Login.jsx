642cd33dd0ae8747120da10d2ef0c48a7089ef83
Rebecca <142409831+extra-jalapenos@users.noreply.github.com>
2024-06-23 21:46:31 +0200

    add note of database migration

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -62,7 +62,12 @@ export default function Login () {
 
   return (
     <div className="center">
-      <h2>Welcome back!</h2>
+      <div className="banner warning">
+        <p><b>Important note – 24th June</b></p>
+        <p>We are currently migrating the database, so you cannot log in. Sorry!</p>
+        <p>We expect to be back the 31th of June, 2024.</p>
+      </div>
+      {/* <h2>Welcome back!</h2>
       <Form name="login" prefill={loginData} handleInput={handleInput} handleSubmit={handleSubmit} />
       {failMessage && <p className="banner warning center">{failMessage}</p>}
       {failedLogin === false && <div className="banner center">
@@ -78,7 +83,7 @@ export default function Login () {
           <button className="red" onClick={resetPage}>Re-try login</button>
         </div>
         )
-      }
+      } */}
     </div>
   )
 }
