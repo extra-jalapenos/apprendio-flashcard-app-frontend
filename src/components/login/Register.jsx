82fc354ab9e31a482d5a7c2abcdb044d9ffc4701
Rebecca <142409831+extra-jalapenos@users.noreply.github.com>
2024-06-25 22:41:55 +0200

    remove commented out code

--- src/components/login/Register.jsx
+++ src/components/login/Register.jsx
@@ -52,13 +52,6 @@ export default function Register () {
 
   return (
     <div className="center">
-      {/* <div className="banner warning">
-        <p><b>Important note</b></p>
-        <p>Due to the app still being under development, we can&apos;t yet accept new users. Thank you for your patience. ♥️</p>
-
-        <p className="fineprint"><b>But why? It <em>is</em> on the internet, isn&apos;t it?</b></p>
-        <p className="fineprint">The main issue is that the backend is not yet connected. The best way to explain what this means is to think of this site as somebody that&apos;s very pretty and knows how to do a lot of cool, amazing things, but has no long-term memory (and no idea about how inappropriate it would be to show your cards to everybody else).</p>
-      </div> */}
       <h2>Hi, so nice to meet you!</h2>
       <p>Create your account and start right away!</p>
       {failMessage && <p className="banner warning center">{failMessage}</p>}
