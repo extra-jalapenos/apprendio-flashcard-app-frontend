4c063b9c89f88af71020b526ab46d18421594f08
Rebecca <142409831+extra-jalapenos@users.noreply.github.com>
2024-07-05 19:06:17 +0200

    change repeat password field name

--- src/components/login/Form.jsx
+++ src/components/login/Form.jsx
@@ -12,7 +12,7 @@ export default function Form ({ prefill, handleSubmit, handleInput }) {
       <p className="formfield-label required">password</p>
       <input className="formfield" name="password" type="password" autoComplete="password" defaultValue={password} />
       <p className="formfield-label required">repeat password</p>
-      <input className="formfield" name="repeat-password" type="repeat-password" />
+      <input className="formfield" name="repeatPassword" type="password" />
       <button value={"Submit"}>Enter</button>
     </form>
   )
