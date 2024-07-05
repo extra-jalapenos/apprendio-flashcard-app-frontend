09ed61a769314a00dc7401d39c7832b21addcbd1
Rebecca <142409831+extra-jalapenos@users.noreply.github.com>
2024-07-05 18:53:36 +0200

    add more fields to register

--- src/components/login/Form.jsx
+++ src/components/login/Form.jsx
@@ -7,8 +7,12 @@ export default function Form ({ prefill, handleSubmit, handleInput }) {
     <form className="form" onChange={handleInput} onSubmit={handleSubmit}>
       <p className="formfield-label required">username</p>
       <input className="formfield" name="username" autoComplete="username" defaultValue={username}/>
+      <p className="formfield-label required">e-mail</p>
+      <input className="formfield" name="email" autoComplete="email" />
       <p className="formfield-label required">password</p>
       <input className="formfield" name="password" type="password" autoComplete="password" defaultValue={password} />
+      <p className="formfield-label required">repeat password</p>
+      <input className="formfield" name="repeat-password" type="repeat-password" />
       <button value={"Submit"}>Enter</button>
     </form>
   )
