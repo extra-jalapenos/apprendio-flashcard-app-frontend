17d13d53fe5b2c2205c747501e2685d0181be80e
Rebecca <142409831+extra-jalapenos@users.noreply.github.com>
2025-01-17 11:42:49 +0100

    change session stats

--- src/components/footer/SessionStatistics.jsx
+++ src/components/footer/SessionStatistics.jsx
@@ -1,30 +1,15 @@
 import { useContext, useEffect } from "react"
 import { sessionContext } from "../../context"
-import { makeHeaders } from "../../helpers/auth"
+import { api } from "../../api/api"
 
 export default function SessionStatistics () {
   const { sessionStats, setSessionStats } = useContext(sessionContext)
 
   const loadTodaysStats = () => {
     const get = async () => {
-      const endpoint = `/api/users/me/statistics/today`
-      const options = {
-        headers: makeHeaders()
-      }
-
-      try {
-        const response = await fetch(endpoint, options)
-        if (response.status === 200) {
-          const data = await response.json()
-          setSessionStats(data.statistic)
-        } else if (response.status === 404) {
-          console.log("no session stats yet")
-        }
-        return
-      } catch (error) {
-        console.log(error)
-        return
-      }
+      const data = await api.getTodaysStats()
+      if (data.message) return
+      setSessionStats(data.statistic)
     }
     get()
   }
@@ -32,8 +17,8 @@ export default function SessionStatistics () {
   useEffect(loadTodaysStats, [setSessionStats])
 
   if (!sessionStats) return (<></>)
-  const correct = sessionStats.correct ? sessionStats.correct : ""
-  const incorrect = sessionStats.incorrect ? sessionStats.incorrect : ""
+  const correct = sessionStats.correct || ""
+  const incorrect = sessionStats.incorrect || ""
   const total = sessionStats ? correct + incorrect : ""
 
   return (
@@ -41,11 +26,8 @@ export default function SessionStatistics () {
       <label>
         {new Date().toLocaleDateString()}
       </label>
-      {/* <label>🥇 Correct</label> */}
       <p className="circlebutton green">{correct}</p>
-      {/* <label>↻ Maybe next time</label> */}
       <p className="circlebutton red">{incorrect}</p>
-      {/* <label>Total</label> */}
       <p className="circlebutton blue">{total}</p>
     </div>
   )
